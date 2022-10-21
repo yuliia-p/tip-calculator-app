3c933e4465793adfb71fe9c9b4d7eee2efd02c5c
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-10-21 21:02:26 +0000

    setup compl

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TipCalc() {
+  return (
+    <h1>Tip calculator app!</h1>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import TipCalc from './app';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<TipCalc />);
